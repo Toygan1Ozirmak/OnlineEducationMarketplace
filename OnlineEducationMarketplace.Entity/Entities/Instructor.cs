a11d260932e923d4143f9e93aafd534fbd4e13ab
toyga <toyga@DESKTOP-I64G8VV>
2023-10-03 10:25:06 +0300

    Proje dosyası ekle.

---
+++ OnlineEducationMarketplace.Entity/Entities/Instructor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineEducationMarketplace.Entity.Entities
+{
+    public class Instructor
+    {
+        public Guid Id { get; set; }
+        public string InstructorName { get; set; }
+
+        
+    }
+}
---
+++ OnlineEducationMarketplace.Entity/Entities/Users.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineEducationMarketplace.Entity.Entities
+{
+    public class Users
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string UserName { get; set; }
+
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+
+    }
+}

38a6f9e634841f34050c0fca9a5f0262e9379a29
GOD <GOD@DESKTOP-1UQONKI>
2023-10-10 09:53:21 +0300

    added

---
+++ OnlineEducationMarketplace.Data/Contracts/ICourseRepository.cs
@@ -0,0 +1,15 @@
+﻿using OnlineEducationMarketplace.Data.Repositories;
+using OnlineEducationMarketplace.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineEducationMarketplace.Data.Contracts
+{
+    public interface ICourseRepository : IRepositoryBase<Course>
+    {
+
+    }
+}

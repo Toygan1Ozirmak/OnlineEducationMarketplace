07553d4c4fba4714be726639754598a194abe8d8
GOD <GOD@DESKTOP-1UQONKI>
2023-10-11 08:58:30 +0300

    revievrepo added

---
+++ OnlineEducationMarketplace.Data/Contracts/IReviewRepository.cs
@@ -0,0 +1,17 @@
+﻿using OnlineEducationMarketplace.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineEducationMarketplace.Data.Contracts
+{
+    public interface IReviewRepository : IRepositoryBase<Review>
+    {
+        IQueryable<User> GetReviewByCourseId(int id, bool trackChanges);
+        void CreateReview(Review review);
+        void UpdateReview(Review review);
+        void DeleteReview(Review review);
+    }
+}
---
+++ OnlineEducationMarketplace.Data/Repositories/ReviewRepository.cs
@@ -0,0 +1,29 @@
+﻿using OnlineEducationMarketplace.Data.Contracts;
+using OnlineEducationMarketplace.Data.NewFolder;
+using OnlineEducationMarketplace.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineEducationMarketplace.Data.Repositories
+{
+    public class ReviewRepository : RepositoryBase<Review>, IReviewRepository
+    {
+        public ReviewRepository(RepositoryContext context) : base(context)
+        {
+        }
+
+        public void CreateReview(Review review) => Create(review);
+
+        public void DeleteReview(Review review) => Delete(review);
+
+        public IQueryable<User> GetReviewByCourseId(int id, bool trackChanges)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateReview(Review review) => Update(review);
+    }
+}

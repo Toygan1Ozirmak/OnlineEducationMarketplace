7feb18170a80f7ddf2b0da097d090bb087da56ca
Toygan Özırmak <toygan.ozirmak@hotmail.com>
2023-11-16 16:39:36 +0300

    authentication last

--- OEMAP.Api/ActionFilters/ValidationFilterAttribute.cs
+++ OEMAP.Api/ActionFilters/ValidationFilterAttribute.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using OnlineEducationMarketplace.Entity.DTOs;
 
 namespace OEMAP.Api.ActionFilters
 {
@@ -10,21 +11,27 @@ namespace OEMAP.Api.ActionFilters
             var controller = context.RouteData.Values["controller"];
             var action = context.RouteData.Values["action"];
 
-            //dto
+            // DTO
             var param = context.ActionArguments
-                .SingleOrDefault(p => p.Value.ToString().Contains("Dto")).Value;
+                .SingleOrDefault(p => p.Value.GetType().Name.Contains("Dto")).Value;
 
-            if (param != null)
+            if (param == null)
             {
-                context.Result = new BadRequestObjectResult($"object is null" +
-                    $"Controller : {controller}" +
-                    $"Action : {action}");//400
-                return;    
+                context.Result = new BadRequestObjectResult($"DTO is null. Controller: {controller}, Action: {action}");
+                return;
             }
 
-            if(!context.ModelState.IsValid)
-                context.Result = new UnprocessableEntityObjectResult(context.ModelState);//422
+            if (!(param is UserForRegistrationDto)) // UserForRegistrationDto yerine doğru DTO tipini ekleyin
+            {
+                context.Result = new BadRequestObjectResult($"Invalid type for DTO. Controller: {controller}, Action: {action}");
+                return;
+            }
 
+            if (!context.ModelState.IsValid)
+            {
+                context.Result = new UnprocessableEntityObjectResult(context.ModelState); // 422
+                return;
+            }
         }
     }
 }
